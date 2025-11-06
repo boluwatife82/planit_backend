e13a4fcdcf2f54ef19b0d87d4bccc7f3dcc76481
Boluwatife <boluwatife82@gmail.com>
2025-11-06 23:47:15 +0100

    update

---
+++ test.js
@@ -0,0 +1,4 @@
+import "dotenv/config"; // automatically loads .env
+
+console.log("FIREBASE_CLIENT_EMAIL:", process.env.FIREBASE_CLIENT_EMAIL);
+console.log("FIREBASE_PROJECT_ID:", process.env.FIREBASE_PROJECT_ID);
